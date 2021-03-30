0756a9042ee587be047b6fe9cf316b88215230dd
Hackbright Student <enidgonzalez602@gmail.com>
2021-03-30 10:52:08 -0700

    add functions for updating played words

--- scrabble_project.py
+++ scrabble_project.py
@@ -13,7 +13,7 @@ letters_to_points = dict(zip(letters, points))
 letters_to_points[" "] = 0
 print(letters_to_points)
 
-#create a function that will take in a word and return how many points that word is worth. If the word does not exist add 0 to the point total. 
+# Create a function that will take in a word and return how many points that word is worth. If the word does not exist add 0 to the point total. 
 
 def score_word(word):
   point_total = 0
@@ -33,17 +33,34 @@ print(f"The total for the word is: {brownie_points}")
 # Create a dictionary from scrated with given players and their scrabble words:
 player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
 
-# Create an empty dictionary for player points
-
+# Gloabally scoped dictionary for points
 player_to_points = {}
+
+# Create function that updates the points total for each player
+def update_points_totals():
 # loop over players and their list of words
-for player, words in player_to_words.items():
-  player_points = 0
-  #Now loop over each word in each players' word list
-  for word in words: 
-    player_points += score_word(word)
-    player_to_points[player] = player_points
-print(f"The total for each player is: {player_to_points}")
+  for player, words in player_to_words.items():
+    player_points = 0
+    #Now loop over each word in each players' word list
+    for word in words: 
+      player_points += score_word(word)
+      player_to_points[player] = player_points
+  print(f"The total for each player is: {player_to_points}")
+
+update_points_totals()
+
+# Create a function that updates the word list each player has played
+
+def play_word(player, word):
+  player_to_words[player].append(word)
+
+#Function call and print statements
+play_word("player1", "CODE")
+print(player_to_words)
+print(player_to_points)
+
+
+
 
 
 
